7baf3c932422a1e08b5534ca18dfb91a94e22109
zeromake <a390720046@gmail.com>
2020-04-09 13:19:56 +0800

    test: add combine test

---
+++ pkg/arrays/combine_test.go
@@ -0,0 +1,29 @@
+package arrays
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestCombine(t *testing.T) {
+	n := [][2]int{
+		{4,2},
+	}
+	result := [][][]int{
+		{
+			{1, 2},
+			{1, 3},
+			{1, 4},
+			{2, 3},
+			{2, 4},
+			{3, 4},
+		},
+	}
+	for i, r := range n {
+		rr := Combine(r[0], r[1])
+		assert.Equal(t, rr , result[i])
+		rr = Combine2(r[0], r[1])
+		assert.Equal(t, rr , result[i])
+	}
+}
+
